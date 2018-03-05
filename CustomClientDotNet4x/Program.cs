34c36db29fd0d16de0fb962f8f89f2adf71ba74a
David Prothero <david@prothero.com>
2018-03-05 15:47:20 -0800

    .NET Framework 4.x example added

---
+++ CustomClientDotNet4x/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace CustomClientDotNet4x
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twilioRestClient = ProxiedTwilioClientCreator.GetClient();
+
+            // Now that we have our custom built TwilioRestClient,
+            // we can pass it to any REST API resource action.
+            var message = MessageResource.Create(
+                to: new PhoneNumber("+15017122661"),
+                from: new PhoneNumber("+15017122661"),
+                body: "Hey there!",
+                // Here's where you inject the custom client
+                client: twilioRestClient
+            );
+
+            Console.WriteLine($"Message SID: {message.Sid}");
+        }
+    }
+}
---
+++ CustomClientDotNet4x/ProxiedTwilioClientCreator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Twilio.Clients;
+
+namespace CustomClientDotNet4x
+{
+    public static class ProxiedTwilioClientCreator
+    {
+        private static HttpClient _httpClient;
+
+        private static void CreateHttpClient()
+        {
+            var proxyUrl = ConfigurationManager.AppSettings["ProxyServerUrl"];
+            var handler = new HttpClientHandler()
+            {
+                Proxy = new WebProxy(proxyUrl),
+                UseProxy = true
+            };
+
+            _httpClient = new HttpClient(handler);
+            var byteArray = Encoding.Unicode.GetBytes(
+                ConfigurationManager.AppSettings["ProxyUsername"] + ":" +
+                ConfigurationManager.AppSettings["ProxyPassword"]
+            );
+
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue(
+                    "Basic", Convert.ToBase64String(byteArray));
+        }
+
+        public static TwilioRestClient GetClient()
+        {
+            var accountSid = ConfigurationManager.AppSettings["TwilioAccountSid"];
+            var authToken = ConfigurationManager.AppSettings["TwilioAuthToken"];
+
+            if (_httpClient == null)
+            {
+                // It's best* to create a single HttpClient and reuse it
+                // * See: https://goo.gl/FShAAe
+                CreateHttpClient();
+            }
+
+            var twilioRestClient = new TwilioRestClient(
+                accountSid,
+                authToken,
+                httpClient: new Twilio.Http.SystemNetHttpClient(_httpClient)
+            );
+
+            return twilioRestClient;
+        }
+    }
+}
