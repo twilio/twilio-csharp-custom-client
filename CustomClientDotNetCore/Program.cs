e956b04e0e6cf4bc501d5a023a5e1166bea00a93
David Prothero <david@prothero.com>
2018-03-05 16:20:40 -0800

    Add .NET Core 2.0 example

---
+++ CustomClientDotNetCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace CustomClientDotNetCore
+{
+    class Program
+    {
+        private static IConfiguration _configuration;
+
+        static void Main(string[] args)
+        {
+            // Bootstrap configuration system. This is usually handled
+            // for you in ASP.NET Core projects.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            _configuration = builder.Build();
+
+            // Create a custom Twilio client
+            var clientCreator = new ProxiedTwilioClientCreator(_configuration);
+            var twilioRestClient = clientCreator.GetClient();
+
+            // Now that we have our custom built TwilioRestClient,
+            // we can pass it to any REST API resource action.
+            var message = MessageResource.Create(
+                to: new PhoneNumber("+15017122661"),
+                from: new PhoneNumber("+15017122661"),
+                body: "Hey there!",
+                // Here's where you inject the custom client
+                client: twilioRestClient
+            );
+
+            Console.WriteLine($"Message SID: {message.Sid}");
+        }
+    }
+}
---
+++ CustomClientDotNetCore/ProxiedTwilioClientCreator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Twilio.Clients;
+
+namespace CustomClientDotNetCore
+{
+    public class ProxiedTwilioClientCreator
+    {
+        private readonly IConfiguration _configuration;
+        private static HttpClient _httpClient;
+
+        public ProxiedTwilioClientCreator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private void CreateHttpClient()
+        {
+            var proxyUrl = _configuration["ProxyServerUrl"];
+            var handler = new HttpClientHandler()
+            {
+                Proxy = new WebProxy(proxyUrl),
+                UseProxy = true
+            };
+
+            _httpClient = new HttpClient(handler);
+            var byteArray = Encoding.Unicode.GetBytes(
+                _configuration["ProxyUsername"] + ":" +
+                _configuration["ProxyPassword"]
+            );
+
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue(
+                    "Basic", Convert.ToBase64String(byteArray));
+        }
+
+        public TwilioRestClient GetClient()
+        {
+            var accountSid = _configuration["TwilioAccountSid"];
+            var authToken = _configuration["TwilioAuthToken"];
+
+            if (_httpClient == null)
+            {
+                // It's best* to create a single HttpClient and reuse it
+                // * See: https://goo.gl/FShAAe
+                CreateHttpClient();
+            }
+
+            var twilioRestClient = new TwilioRestClient(
+                accountSid,
+                authToken,
+                httpClient: new Twilio.Http.SystemNetHttpClient(_httpClient)
+            );
+
+            return twilioRestClient;
+        }
+    }
+}
